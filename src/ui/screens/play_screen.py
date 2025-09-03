816c728025cac3b6f6ad2ef60de7bad82ab9f949
Benjamin Kozlowski <bkozlowski@ku.edu>
2025-09-03 16:27:26 -0500

    fixed adjacency logic

--- src/ui/screens/play_screen.py
+++ src/ui/screens/play_screen.py
@@ -34,6 +34,17 @@ class PlayScreen:
 
         pg.display.update()
 
+    def adjacent_indices(self, cell):
+        target_index = self.grid.index(cell)
+        left_edge = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+        right_edge = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+        if (target_index in left_edge):
+            return [target_index + 1, target_index + 10, target_index + 11, target_index - 9, target_index - 10]
+        elif (target_index in right_edge):
+            return [target_index - 1, target_index + 10, target_index + 9, target_index - 11, target_index - 10]
+        else:
+            return [target_index - 1, target_index + 1, target_index + 10, target_index - 10, target_index + 9, target_index + 11, target_index - 9, target_index - 11]
+    
     def set_mines(self):
         for i in range(self.num_mines):
             target_cell = rd.choice(self.grid)
@@ -41,22 +52,20 @@ class PlayScreen:
                 target_cell = rd.choice(self.grid)
             target_cell.set_mine()
             #increment adjacent mine counts
-            target_index = self.grid.index(target_cell)
-            adjacent_indices = [target_index - 1, target_index + 1, target_index - 11, target_index + 11, target_index - 10, target_index + 10, target_index -9, target_index + 9]
+            adjacent_indices = self.adjacent_indices(target_cell)
             for index in adjacent_indices:
                 if 0 <= index < len(self.grid):
                     self.grid[index].increment_adjacent_mines()
 
-    def uncover_adjacent_cells(cell, grid):
+    def uncover_adjacent_cells(self, cell, grid):
         if cell.adjacent_mines == 0 and not cell.is_mine:
-            target_index = grid.index(cell)
-            adjacent_indices = [target_index - 1, target_index + 1, target_index - 11, target_index + 11, target_index - 10, target_index + 10, target_index -9, target_index + 9]
+            adjacent_indices = self.adjacent_indices(cell)
             for index in adjacent_indices:
                 if 0 <= index < len(grid):
                     adjacent_cell = grid[index]
                     if adjacent_cell.is_covered and not adjacent_cell.is_mine:
                         adjacent_cell.uncover()
-                        PlayScreen.uncover_adjacent_cells(adjacent_cell, grid)
+                        self.uncover_adjacent_cells(adjacent_cell, grid)
 
     def handle_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
@@ -69,4 +78,5 @@ class PlayScreen:
                 #   self.play_state = "game_over"
                 #  print("Game Over")
                 #else:
-                PlayScreen.uncover_adjacent_cells(cell, self.grid)
+                self.uncover_adjacent_cells(cell, self.grid)
+                print(f"Cell at index {self.grid.index(cell)} uncovered with {cell.adjacent_mines} adjacent mines.")
