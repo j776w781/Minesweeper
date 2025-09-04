483136e86d1731da1e065611010928eb30f6fa08
willbhedges <willbhedges@icloud.com>
2025-09-04 10:57:22 -0500

    Update Minesweeper Colors to Green Background and Brown Uncovered

--- src/model/cell.py
+++ src/model/cell.py
@@ -36,13 +36,13 @@ class Cell:
     def draw(self, surface, x, y, size=40):
         rect = pg.Rect(x, y, size, size)
         if self.is_covered: #covered cell drawing
-            pg.draw.rect(surface, (100, 100, 100), rect, width=0)
+            pg.draw.rect(surface, (53, 252, 3), rect, width=0)
             pg.draw.rect(surface, (0, 0, 0), rect, width=1)
             self.rect = rect
             if self.is_flagged:
                 pg.draw.circle(surface, (255, 0, 0), rect.center, 10)
         else: #uncovered cell drawing
-            pg.draw.rect(surface, (0, 0, 255), rect, width=0)
+            pg.draw.rect(surface, (128, 123, 78), rect, width=0)
             self.rect = rect
             if self.is_mine:
                 pg.draw.rect(surface, (255, 0, 0), rect, width=0)
