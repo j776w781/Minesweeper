f85028f53ed4597eb33828dfd963a51fa0cc60f1
ruth-higgason <rhiggason@ku.edu>
2025-09-11 18:01:06 -0500

    remove unused variables

--- src/game/settings.py
+++ src/game/settings.py
@@ -8,20 +8,17 @@ Authors: Benjamin Kozlowski
 Creation date: 28 August 2025
 """
 import pygame
-
 GRID = 10
 TILE_PX = 48
-MARGIN = 16
 HUD_H = 64
+MARGIN = 16
 WIDTH = MARGIN*2 + GRID*TILE_PX
 HEIGHT = MARGIN*2 + GRID*TILE_PX + HUD_H
 FPS = 60
-#variables used for board.py
 BOX_WIDTH = 400
 BOX_HIGHT = 400
 WHITE = (200, 200, 200)
 BLACK = (0, 0, 0)
-GRAY = (100, 100, 100)
 BOX_X = (WIDTH-BOX_WIDTH)//2
 BOX_Y = (HEIGHT-BOX_HIGHT)//2
 BLOCKSIZE = 40
